976a30461227e000f08f9420cc821c0e70262d56
iradakhf <iradakhf@code.edu.az>
2022-09-12 10:15:18 +0400

    changes added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ var output = document.getElementById("demo");
 var output2 = document.getElementById("demo2");
 var output3 = document.getElementById("demo3");
 var loanAmount= document.getElementById("loan");
-
+var total =0;
+var interest = 0;
 output.innerHTML = slider.value;
 output2.innerHTML = slider2.value;
 output3.innerHTML = slider3.value;
@@ -18,21 +19,21 @@ var rate = output3.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
     console.log (output.innerHTML);
-    amount=  Number(output.innerHTML);
+    amount=  parseFloat(output.innerHTML);
     console.log (amount);
 }
 slider2.oninput = function() {
     output2.innerHTML = this.value;
     console.log (output2.innerHTML);
-    months = Number(output2.innerHTML);
+    months = parseFloat(output2.innerHTML);
 }
 slider3.oninput = function() {
     output3.innerHTML = this.value;
-    rate= Number(output3.innerHTML);
+    rate= parseFloat(output3.innerHTML);
 }
 function Calculate() {
-var interest = (amount * (rate * 0.01)) / months;
+interest = (amount * (rate * 0.01)) / months;
 console.log (interest);
-var total = ((amount / months) + interest);
+total = ((amount / months) + interest);
     loanAmount.innerHTML = total;
 }  
